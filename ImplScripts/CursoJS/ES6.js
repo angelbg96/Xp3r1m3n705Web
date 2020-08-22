ce0d55132789e0ac54f5723c6d7237046918e48e
angelbg96 <angelbg96.28@gmail.com>
2020-08-21 21:22:44 -0500

    curiosidades de ES6

---
+++ ImplScripts/CursoJS/ES6.js
@@ -0,0 +1,20 @@
+const condicionalCorto = () => {
+    // si devuelve un TRUE, ejecutara la funcion de la derecha
+    getBoolean() && console.log("Me ejecute")
+}
+
+const getBoolean = (sup = 100, inf = 0) => {
+    let numR = Math.random()
+    numR = Math.round(numR * sup + inf )
+    console.log(`El número es ${numR}`)
+    return numR % 2 === 0 ? true : false
+}
+
+const randomOrder = () => {
+    let arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    console.log(arreglo.sort(() =>{ return Math.random() -0.5 }))
+}
+
+condicionalCorto()
+
+randomOrder()
